02e46f7e440505c32b6f4ed540f7514cd58db581
Aaryan10-sudo <144255729+Aaryan10-sudo@users.noreply.github.com>
2024-09-23 08:30:59 +0545

    Update connectMongo.js

--- src/connectDatabase/connectMongo.js
+++ src/connectDatabase/connectMongo.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 let connectDB = () => {
-  mongoose.connect("mongodb://localhost:27017/mernbackend");
+  mongoose.connect("*********************");
   console.log("Connect to Database mernbackend");
 };
 export default connectDB;
